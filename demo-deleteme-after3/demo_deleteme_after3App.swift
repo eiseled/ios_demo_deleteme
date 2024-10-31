a46d13132606b7bc570fa3ec0953db1f57d0e515
eiseled <124140755+eiseled@users.noreply.github.com>
2024-10-31 08:39:02 -0400

    start of the project

---
+++ demo-deleteme-after3/demo_deleteme_after3App.swift
@@ -0,0 +1,17 @@
+//
+//  demo_deleteme_after3App.swift
+//  demo-deleteme-after3
+//
+//  Created by Dennis Eisele on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct demo_deleteme_after3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
